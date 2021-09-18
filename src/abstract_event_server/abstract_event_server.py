3221a3a2ab8e3e4aa6ddd434404981d4096ce625
John Janecek <janecektyler@gmail.com>
2021-09-18 10:18:58 -0600

    Use SSL

--- src/abstract_event_server/abstract_event_server.py
+++ src/abstract_event_server/abstract_event_server.py
@@ -5,13 +5,15 @@ from aiohttp import web
 import aiohttp
 import asyncio
 
+import ssl
+
 import os
 from time import sleep
 import json
 import requests
 
 class AbstractEventServer():
-	def __init__(self, port: int, ip='0.0.0.0'):
+	def __init__(self, ip='0.0.0.0', port=65000, public_key='public.pem', private_key='private.key'):
 		self.__logger = logging.getLogger("server")
 		formatter = logging.Formatter('%(asctime)s SRVR | %(levelname)s | %(message)s')
 		sh = logging.StreamHandler()
@@ -20,8 +22,10 @@ class AbstractEventServer():
 		self.__logger.addHandler(sh)
 		self.__logger.setLevel(logging.DEBUG)
 
-		self._ip = ip
-		self._port = port
+		self.__ip = ip
+		self.__port = port
+		self.__public_key = public_key
+		self.__private_key = private_key
 
 	async def process(self, data: dict):
 		return "Default response"
@@ -41,10 +45,13 @@ class AbstractEventServer():
 
 		runner = web.AppRunner(app)
 		await runner.setup()
-		site = web.TCPSite(runner, self._ip, self._port)    
+
+		ssl_context = ssl.create_default_context(ssl.Purpose.CLIENT_AUTH)
+		ssl_context.load_cert_chain(self.__public_key, self.__private_key)
+		site = web.TCPSite(runner, self.__ip, self.__port, ssl_context=ssl_context)
 		await site.start()
 
-		self.__logger.info(f"Serving on ('{self._ip}', {self._port}) ...")
+		self.__logger.info(f"Serving on ('{self.__ip}', {self.__port}) ...")
 
    	 	# wait forever
 		await asyncio.Event().wait()
@@ -53,5 +60,5 @@ class AbstractEventServer():
 		asyncio.run(self.main())
 
 if __name__ == '__main__':
-	a = AbstractServer(65000)
+	a = AbstractEventServer(public_key='/home/fourbolt/Documents/resume_formatter/keys/test.pem', private_key='/home/fourbolt/Documents/resume_formatter/keys/test.key')
 	a.start()
